a328450b551ca129d0825fb572986d063bb33d40
yaser alhamad <yaser.alhamadwork@gmail.com>
2022-09-15 18:34:19 +0300

    Updates

---
+++ precache-manifest.a1e1c1029a5e2cfe38264171b2418f98.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1a3a2f67e6ad72599149c09ba0f0fc9d",
+    "url": "/To-Do/index.html"
+  },
+  {
+    "revision": "6765ca38a5051e549036",
+    "url": "/To-Do/static/js/2.e3c96499.chunk.js"
+  },
+  {
+    "revision": "e56346cff443c52c3e0c",
+    "url": "/To-Do/static/js/main.7b616ce9.chunk.js"
+  },
+  {
+    "revision": "c0bcd702e11d36f5f5c0",
+    "url": "/To-Do/static/js/runtime-main.4a60cdf7.js"
+  }
+]);
